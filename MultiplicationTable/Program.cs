4ac5c08018c0661fd918b855677c13555e6b74a8
micke_kappa@hotmail.com <micke_kappa@hotmail.com>
2019-06-24 11:43:30 +0200

    Test

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stage1(3);
+            MultiplicationTable(3,10);
+        }
+
+        private static void Stage1(int maxA)
+        {
+            for (int i = 1; i <= maxA; i++)
+            {
+                Console.WriteLine($"{i} * {maxA} = {i * maxA}");
+            }
+        }
+
+        private static void MultiplicationTable(int maxA, int maxB)
+        {
+            for (int i = 1 ; i <= maxA; i++)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+                Console.WriteLine("\u250f\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                                   "\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                                   "\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                                   "\u2501\u2501\u2501\u2501\u2513");
+                Console.WriteLine($"\u2503 Multiplication table for {i}  \u2503");
+                Console.WriteLine("\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                    "\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                    "\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2501" +
+                    "\u2501\u2501\u2501\u2501\u251b");
+
+                for (int a = 1; a <= maxB; a++)
+                {
+                    
+                    Console.WriteLine($"{i} * {a} = {i * a}");
+                    
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
